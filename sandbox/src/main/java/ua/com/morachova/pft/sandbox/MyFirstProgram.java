726308a9ab893acbb5bc4f0a4cabf23dd4731437
Maria Morachova <m.morachova@gmail.com>
2016-04-11 20:10:42 +0300

    Code auto-reformat

--- sandbox/src/main/java/ua/com/morachova/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/com/morachova/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ua.com.morachova.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main (String[] args){
-		System.out.println("Hello world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
