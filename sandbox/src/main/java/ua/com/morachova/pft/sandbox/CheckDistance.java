24cb26f536fc0c27548e696c304c551982d54be3
Maria Morachova <m.morachova@gmail.com>
2016-04-11 21:27:06 +0300

    Added constructor and examples

--- sandbox/src/main/java/ua/com/morachova/pft/sandbox/CheckDistance.java
+++ sandbox/src/main/java/ua/com/morachova/pft/sandbox/CheckDistance.java
@@ -3,13 +3,14 @@ import static ua.com.morachova.pft.sandbox.Point.distance;
 
 public class CheckDistance {
   public static void main(String[] args){
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x=5.0;
-    p1.y=9.0;
-    p2.x=8.0;
-    p2.y=12.0;
+    Point p1 = new Point(5.0, 9.0);
+    Point p2 = new Point(8.0, 12.0);
+    Point p3 = new Point(1.2, 3.7);
+    Point p4 = new Point(7.3, 9.8);
 
     distance(p1, p2);
+    distance(p3, p4);
+    distance(p1, p4);
+    distance(p4, p2);
   }
 }
--- sandbox/src/main/java/ua/com/morachova/pft/sandbox/Point.java
+++ sandbox/src/main/java/ua/com/morachova/pft/sandbox/Point.java
@@ -4,6 +4,10 @@ public class Point {
   double x;
   double y;
   static double d;
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
 
   public static double distance(Point p1, Point p2){
     d = Math.sqrt((p2.x - p1.x) + (p2.y - p1.y));
