6468dab17b8d86287df04d51c04852514be02078
Maria Morachova <m.morachova@gmail.com>
2016-04-22 18:37:12 +0300

    Added default values check

--- addressbook-web-tests/src/test/java/ua/com/morachova/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/com/morachova/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("test1", "testing1", "kievcity", "test1.testing1@mail.net"));
+    app.getContactHelper().fillContactForm(new ContactData("test1", null, "kievcity", null));
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().gotoHomePage();
   }
--- addressbook-web-tests/src/test/java/ua/com/morachova/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/com/morachova/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("name1", "header1", "footer1"));
+    app.getGroupHelper().fillGroupForm(new GroupData("name1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
